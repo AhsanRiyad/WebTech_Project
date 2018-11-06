00a9b52502a9ad26b0f1852ba4c6e21584a7bcd4
AhsanRiyad <riyad298@gmail.com>
2018-11-06 09:47:39 +0600

    post super globals checking and function checking ends and deleted all files

--- index.php
+++ index.php
@@ -15,70 +15,15 @@ include 'linker_files/head.html';
 <!-- Add your site or application content here -->
 
 
-<!-- checking php post super globals -->
 
 
+<!-- body should end after this statement -->
 <?php 
 
-$_POST['hellow'] = 'riyad';
-
-
-echo $_POST['hellow'];
-	//its working
-
-
-function f1()
-{
-	return 2;
-}
-
-
-if (2==2) {
-	$k = f1();
-	echo $k;
-}
-
-
-
-
-
-
-
-
-
-
+include 'linker_files/tail.html';
 
 
 ?>
-
-<form method="post" action="other.php">
-
-	<input type="text" name="riyad" value="nothing">
-
-	<input type="submit" name="submit" value="submit" >
-
-	<input type="button" name="" value="submit">
-
-
-</form>
-
-<!-- summery : tried different way but I could not submit a form without using a form;
-	javascript can come up with a support for this issue -->
-
-
-
-
-
-
-
-
-	<!-- body should end after this statement -->
-	<?php 
-
-	include 'linker_files/tail.html';
-
-
-	?>
 
 
 
--- other.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-print_r($_POST);
-//does not work if the form is not submitted
-
- ?>
