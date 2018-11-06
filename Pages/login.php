162e9290cd2d70cf351f9bb5982743c85abd3a22
AhsanRiyad <riyad298@gmail.com>
2018-11-06 11:29:43 +0600

    f

--- Pages/login.php
+++ Pages/login.php
@@ -1,28 +1,15 @@
-
-<!-- body should start after this statement -->
 <?php 
-
 include 'linker_files/head.html';
-
-
 ?>
-
-
-  <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-<![endif]-->
-
-<!-- Add your site or application content here -->
+<!-- body starts-->
 
 
 
 
-<!-- body should end after this statement -->
+
+<!-- body ends-->
 <?php 
-
 include 'linker_files/tail.html';
-
-
 ?>
 
 
