01e2f37bd06c94664abc62aae8e6b2fdbdff8a99
AhsanRiyad <riyad298@gmail.com>
2018-11-08 00:34:53 +0600

    msg table added

---
+++ admin_home.php
@@ -0,0 +1,45 @@
+<?php 
+include 'linker_files/head.html';
+?>
+<!-- body starts here -->
+
+<h1 align="center">Welcome Admin</h1>
+
+
+<!--product category added -->
+<br>
+<br>
+<h1 class="catergory_color">Admin Control</h1>
+<br>
+<div class="container-fluid admin_background">
+	<div class="row">
+
+			
+
+		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="" class="btn btn-info"><h3>View Message</h3></a></div>
+		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="" class="btn btn-info"><h3>New Product Request</h3></a></div>
+		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="" class="btn btn-info"><h3>Order Request</h3></a></div>
+		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="" class="btn btn-info"><h3>Carousel Image Control</h3></a></div>
+
+		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="" class="btn btn-info"><h3>Order Statistics</h3></a></div>
+
+
+		
+		
+	</div>
+</div>
+
+
+
+<!-- body ends here -->
+<?php 
+include 'linker_files/tail.html';
+?>
+
+
+
+
+
+
+
+
---
+++ msg_req.php
@@ -0,0 +1,62 @@
+<?php 
+include 'linker_files/head.html';
+?>
+<!-- body starts here -->
+
+<div class="container">
+	<table class="table">
+	  <thead class="thead-dark">
+	    <tr>
+	      <th scope="col">#</th>
+	      
+	      <th scope="col">From</th>
+	      <th scope="col">Subject</th>
+	      <th scope="col">Date</th>
+	      <th scope="col">Details</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <tr>
+	      <th scope="row">1</th>
+	      
+	      <td>riyad@gmail.com</td>
+	      <td>order help</td>
+	      <td>10/12/18</td>
+	      <td><a href="#" class="btn btn-info">Details</a></td>
+	    </tr>
+	    <tr>
+	      <th scope="row">2</th>
+	      
+	      <td>riyad@gmail.com</td>
+	      <td>order help</td>
+	      <td>10/12/18</td>
+	      <td><a href="#" class="btn btn-info">Details</a></td>
+	    </tr>
+	    <tr>
+	      <th scope="row">3</th>
+	      <td>riyad@gmail.com</td>
+	      <td>order help</td>
+	      <td>10/12/18</td>
+	      <td><a href="#" class="btn btn-info">Details</a></td>
+	    </tr>
+	  </tbody>
+	</table>
+	
+	
+	  </tbody>
+	</table>
+</div>
+
+
+<!-- body ends here -->
+<?php 
+include 'linker_files/tail.html';
+?>
+
+
+
+
+
+
+
+
