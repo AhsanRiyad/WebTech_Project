149ebbf1c9d07c299de2628108204c3eae2ffd87
AhsanRiyad <riyad298@gmail.com>
2018-11-07 22:53:57 +0600

    using cdn for relationship of files

---
+++ fron_page.php
@@ -0,0 +1,60 @@
+<?php 
+include 'linker_files/head.html';
+?>
+<!-- body starts here -->
+
+<!-- carousel starts -->
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+			  <div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <img class="d-block w-100" src="img/product1.jpg" alt="First slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="img/product2.jpg" alt="Second slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="img/product3.jpg" alt="Third slide">
+			    </div>
+			  </div>
+			  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- body ends here -->
+<?php 
+include 'linker_files/tail.html';
+?>
+
+
+
+
+
+
+
+
