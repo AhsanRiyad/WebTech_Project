a3a1b4f5a292f5024f62278b93b5965247e92fdc
AhsanRiyad <riyad298@gmail.com>
2018-11-08 09:45:07 +0600

    finalize

---
+++ order_req.php
@@ -0,0 +1,66 @@
+<?php 
+include 'linker_files/head.html';
+?>
+<!-- body starts here -->
+
+<div class="container">
+	<table class="table">
+	  <thead class="thead-dark">
+	    <tr>
+	      <th scope="col">#</th>	      
+	      <th scope="col">User</th>
+	      <th scope="col">Amount</th>
+	      <th scope="col">Date</th>
+	      <th scope="col">Details</th>
+	      <th scope="col">Approve</th>
+	      <th scope="col">Deny</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <tr>
+	      <th scope="row">1</th>
+	      
+	      <td>riyad@gmail.com</td>
+	      <td>1200</td>
+	      <td>10/12/18</td>
+	      <td><a href="msg_details.php" class="btn btn-info">Details</a></td>
+	      <td><a href="msg_details.php" class="btn btn-success">Approve</a></td>
+	      <td><a href="msg_details.php" class="btn btn-danger">Deny</a></td>
+	    </tr>
+	    <tr>
+	      <th scope="row">2</th>
+	      
+	      <td>riyad@gmail.com</td>
+	      <td>1200</td>
+	      <td>10/12/18</td>
+	      <td><a href="msg_details.php" class="btn btn-info">Details</a></td>
+	      <td><a href="msg_details.php" class="btn btn-success">Approve</a></td>
+	      <td><a href="msg_details.php" class="btn btn-danger">Deny</a></td>
+	    </tr>
+	    <tr>
+	      <th scope="row">3</th>
+	      <td>riyad@gmail.com</td>
+	      <td>1200</td>
+	      <td>10/12/18</td>
+	      <td><a href="msg_details.php" class="btn btn-info">Details</a></td>
+	      <td><a href="msg_details.php" class="btn btn-success">Approve</a></td>
+	      <td><a href="msg_details.php" class="btn btn-danger">Deny</a></td>
+	    </tr>
+	  </tbody>
+	</table>
+	
+</div>
+
+
+<!-- body ends here -->
+<?php 
+include 'linker_files/tail.html';
+?>
+
+
+
+
+
+
+
+
---
+++ order_stat.php
@@ -0,0 +1,59 @@
+<?php 
+include 'linker_files/head.html';
+?>
+<!-- body starts here -->
+
+<div class="container">
+	<table class="table">
+	  <thead class="thead-dark">
+	    <tr>
+	      <th scope="col">#</th>
+	      
+	      <th scope="col">Time</th>
+	      <th scope="col">Total Order</th>
+	      <th scope="col">Total Amount</th>
+	      <th scope="col">Details</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <tr>
+	      <th scope="row">1</th>
+	      
+	      <td>Today</td>
+	      <td>500</td>
+	      <td>120452</td>
+	      <td><a href="msg_details.php" class="btn btn-info">Details</a></td>
+	    </tr>
+	    <tr>
+	      <th scope="row">2</th>
+	      
+	      <td>January</td>
+	      <td>500</td>
+	      <td>120452</td>
+	      <td><a href="msg_details.php" class="btn btn-info">Details</a></td>
+	    </tr>
+	    <tr>
+	      <th scope="row">3</th>
+	      <td>February</td>
+	      <td>500</td>
+	      <td>120452</td>
+	      <td><a href="msg_details.php" class="btn btn-info">Details</a></td>
+	    </tr>
+	  </tbody>
+	</table>
+	
+</div>
+
+
+<!-- body ends here -->
+<?php 
+include 'linker_files/tail.html';
+?>
+
+
+
+
+
+
+
+
