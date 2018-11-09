ca3265a3d85730c7ecf9729d4cc22ac08332782c
AhsanRiyad <riyad298@gmail.com>
2018-11-10 01:26:04 +0600

    stable header 1

--- linker_files/head.php
+++ linker_files/head.php
@@ -23,51 +23,51 @@
 </head>
 <body>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-2"><a href="index.php"><img src="img/logo.png"></a></div>
-      <div class="offset-md-0 col-6 div_margin">
 
-        <form action="search_result.php">
-          <div class="form-row align-items-center">
-            <div class="col-9">
-              <div class="input-group mb-2">
-                <div class="input-group-prepend">
-                  <div class="input-group-text"><i class="fas fa-search"></i></div>
-                </div>
-                <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Search">
+    <div class="container-fluid bg-light">
+      <div class="row">
+        <div class="col">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-2 col-12 justify-content-md-start d-flex justify-content-center"><a href="index.php"><img src="img/logo.png"></a>
               </div>
+
+              <div class="col-md-4 col-lg-6 col-12 align-self-md-center">
+          
+                <form action="search_result.php">
+                  <div class="form-row align-items-center">
+                    <div class="col-9">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <div class="input-group-text"><i class="fas fa-search"></i></div>
+                        </div>
+                        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Search">
+                      </div>
+                    </div>
+
+                    <div class="col-3">
+                      <button type="submit" class="btn btn-primary">Search</button>
+                    </div>
+                  </div>
+                </form> 
+              </div>
+            <div class="col-md-2 col-lg-1 col-3 offset-1 mt-3 mb-3 mb-md-0 mt-md-0 offset-md-0 d-flex justify-content-center align-self-md-center"><a href="login.php" class="btn btn-success"> Sign In</a></div>
+          
+            <div class="col-md-2  col-lg-1 col-3 mt-3 mb-3 mb-md-0 mt-md-0 d-flex justify-content-center align-self-md-center"><a href="contact.php" class="btn btn-info">Need Help?</a></div>
+          
+          
+            <div class="col-md-2  col-lg-1 col-3 mt-3 mb-3 mb-md-0 mt-md-0 d-flex justify-content-center align-self-md-center">
+              <a href="cart.php" class="btn btn-danger text-link">
+                Cart <span class="badge badge-light">4</span>
+              </a>
             </div>
-            <div class="col-3">
-              <button type="submit" class="btn btn-primary mb-2">Search</button>
-            </div>
+          
+          
           </div>
-        </form>
-
-
+            </div>
+        </div>
       </div>
-      
-
-
-      
-
-
-
-      <div class="col div_margin"><a href="login.php" class="btn btn-primary"> <i class="fas fa-stroopwafel"></i> Sign In</a></div>
-
-      <div class="col div_margin"><a href="contact.php" class="btn btn-primary">Need Help?</a></div>
-
-
-      <div class="col div_margin">
-        <a href="cart.php" class="btn btn-primary">
-          Cart <span class="badge badge-light">4</span>
-        </a>
-      </div>
-
-
     </div>
-  </div>
-
 
 
   
