628728591ff0764584b3a512b814b40502daf11c
AhsanRiyad <riyad298@gmail.com>
2018-11-10 12:33:34 +0600

    stable category and product

--- index.php
+++ index.php
@@ -40,42 +40,224 @@ include 'linker_files/head.php';
 
 
 <!--product category added -->
-<br>
-<br>
-<br>
-<div class="container">
-	<div class="row">
-		<h1>Category</h1>
-		<div class="w-100"></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>
-		<div class="image_div"><img class="imageOverlay2" src="img/cat1.jpg" alt=""></div>		
+<div class="container-fluid">
+	<div class="row bg-light ">
+		<div class="col">
+			<div class="container">
+				<div class="row">
+					<h3 class="pt-5">Categories</h3>
+					<div class="w-100"></div>
+					<div class="image_div text-info text-center text-info"><img class="imageOverlay2" src="img/cat1.jpg" alt=""> <br> <p>Mobile</p> </div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>	
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>		
+					<div class="image_div text-info text-center"><img class="imageOverlay2" src="img/cat1.jpg" alt=""><br><p class="">Mobile</p></div>	
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
 
 <!--New product category added -->
-<br>
-<br>
-<br>
-<div class="container">
-	<div class="row">
-		<h1>New Products</h1>
-		<div class="col"><img src="img/new_product.jpg" alt=""><br/><a href="product_details.php" class="btn btn-info"><h3>Product Details</h3></a></div>
-		<div class="col"><img src="img/new_product.jpg" alt=""><br/><a href="product_details.php" class="btn btn-info"><h3>Product Details</h3></a></div>
-		<div class="col"><img src="img/new_product.jpg" alt=""><br/><a href="product_details.php" class="btn btn-info"><h3>Product Details</h3></a></div>
-		<div class="col"><img src="img/new_product.jpg" alt=""><br/><a href="product_details.php" class="btn btn-info"><h3>Product Details</h3></a></div>
-		
-		
+<div class="container-fluid">
+	<div class="row bg-light">
+		<div class="col">
+			<div class="container">
+				<div class="row pt-5">
+					<h3>Just In</h3>
+					<div class="w-100"></div>
+					<div class="col-md-2  ">
+						<div class="w_p bg-white ">
+							<img class="img-fluid" src="img/cat1.jpg" alt="">
+
+							<div class="w-100 pl-2">
+								<h6>Mobile phones</h6>
+								<h6 class="text-danger">$2490</h6>
+								<p class="text-danger"> 
+									<strike class="text-muted">$5000
+									</strike>
+									<small class="text-muted"> 39%</small>
+								</p>
+								<div class="w-100 m_t pb-3">
+									
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star"></span>
+									
+								</div>
+								
+
+							</div>
+
+						</div>
+						
+					</div>
+					
+					<div class="col-md-2  ">
+						<div class="w_p bg-white ">
+							<img class="img-fluid" src="img/cat1.jpg" alt="">
+
+							<div class="w-100 pl-2">
+								<h6>Mobile phones</h6>
+								<h6 class="text-danger">$2490</h6>
+								<p class="text-danger"> 
+									<strike class="text-muted">$5000
+									</strike>
+									<small class="text-muted"> 39%</small>
+								</p>
+								<div class="w-100 m_t pb-3">
+									
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star"></span>
+									
+								</div>
+								
+
+							</div>
+
+						</div>
+						
+					</div>
+
+					<div class="col-md-2  ">
+						<div class="w_p bg-white ">
+							<img class="img-fluid" src="img/cat1.jpg" alt="">
+
+							<div class="w-100 pl-2">
+								<h6>Mobile phones</h6>
+								<h6 class="text-danger">$2490</h6>
+								<p class="text-danger"> 
+									<strike class="text-muted">$5000
+									</strike>
+									<small class="text-muted"> 39%</small>
+								</p>
+								<div class="w-100 m_t pb-3">
+									
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star"></span>
+									
+								</div>
+								
+
+							</div>
+
+						</div>
+						
+					</div>
+					<div class="col-md-2  ">
+						<div class="w_p bg-white ">
+							<img class="img-fluid" src="img/cat1.jpg" alt="">
+
+							<div class="w-100 pl-2">
+								<h6>Mobile phones</h6>
+								<h6 class="text-danger">$2490</h6>
+								<p class="text-danger"> 
+									<strike class="text-muted">$5000
+									</strike>
+									<small class="text-muted"> 39%</small>
+								</p>
+								<div class="w-100 m_t pb-3">
+									
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star"></span>
+									
+								</div>
+								
+
+							</div>
+
+						</div>
+						
+					</div>
+
+					<div class="col-md-2  ">
+						<div class="w_p bg-white ">
+							<img class="img-fluid" src="img/cat1.jpg" alt="">
+
+							<div class="w-100 pl-2">
+								<h6>Mobile phones</h6>
+								<h6 class="text-danger">$2490</h6>
+								<p class="text-danger"> 
+									<strike class="text-muted">$5000
+									</strike>
+									<small class="text-muted"> 39%</small>
+								</p>
+								<div class="w-100 m_t pb-3">
+									
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star"></span>
+									
+								</div>
+								
+
+							</div>
+
+						</div>
+						
+					</div>
+
+					<div class="col-md-2  ">
+						<div class="w_p bg-white ">
+							<img class="img-fluid" src="img/cat1.jpg" alt="">
+
+							<div class="w-100 pl-2">
+								<h6>Mobile phones</h6>
+								<h6 class="text-danger">$2490</h6>
+								<p class="text-danger"> 
+									<strike class="text-muted">$5000
+									</strike>
+									<small class="text-muted"> 39%</small>
+								</p>
+								<div class="w-100 m_t pb-3">
+									
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star checked"></span>
+									<span class="fa fa-star"></span>
+									
+								</div>
+								
+
+							</div>
+
+						</div>
+						
+					</div>
+					
+
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
+
 
 
 <!--Best Selling product category added -->
