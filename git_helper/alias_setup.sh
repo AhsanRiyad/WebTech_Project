f93caf03dc98752c4ca0dc2f620f9311b0ad1f2e
AhsanRiyad <riyad298@gmail.com>
2018-11-11 18:02:27 +0600

    commit by alias
    this is used for short cut puspose

--- git_helper/alias_setup.sh
+++ git_helper/alias_setup.sh
@@ -2,4 +2,4 @@ alias add='git add -A'
 alias commit='git commit -m "commit by alias" -m "this is used for short cut puspose"'
 alias push='git push --all origin'
 alias all='git add -A; git commit -m "commit by alias" -m "this is used for short cut puspose"; git push --all origin'
-echo 'riyad'
+clear
