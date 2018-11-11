457c3d1315809690b2b1b7794b4ab7722e3bfdd5
AhsanRiyad <riyad298@gmail.com>
2018-11-12 01:41:08 +0600

    not much stable

--- admin_home.php
+++ admin_home.php
@@ -1,32 +1,96 @@
 <?php 
-include 'linker_files/head.php';
+include 'linker_files/head_admin.php';
 ?>
 <!-- body starts here -->
 
-<h1 align="center">Welcome Admin</h1>
-
-
-<!--product category added -->
-<br>
-<br>
-<h1 class="catergory_color">Admin Control</h1>
-<br>
-<div class="container-fluid admin_background">
+<div class="container-fluid fixed-top position-absolute">
 	<div class="row">
-		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="msg_req.php" class="btn btn-info"><h3>View Message</h3></a></div>
-		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="product_req.php" class="btn btn-info"><h3>New Product Request</h3></a></div>
-		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="order_req.php" class="btn btn-info"><h3>Order Request</h3></a></div>
-		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="" class="btn btn-info"><h3>Slider Image Control</h3></a></div>
+		<!-- fixed horizontal -->
+		<div class="offset-2 col-10  admin_navbar_horizontal bg-secondary d-flex justify-content-center align-items-center">
+			<p class="text-light h3 ">Welcome admin</p>
+		</div>
+	</div>
+</div>
 
-		<div class="col"><img src="img/admin.jpg" alt=""><br/><a href="order_stat.php" class="btn btn-info"><h3>Order Statistics</h3></a></div>	
+<div class="container-fluid position-absolute">
+	<div class="row">
+
+		<!-- fixed vertical -->
+		<div class=" admin_navbar_veritcal bg-dark col-2 px-0">
+			<div class="w-100 bg-light d-flex justify-content-center">
+				<img class=" py-1" src="img/logo_dashboard.png" alt="">
+
+			</div>
+
+			<a class="text-white" href="#">
+				<div class="w-100 bg-info py-2 text-center my-4">
+
+				<i class="fas fa-tachometer-alt"></i> Dashboard
+
+			</div></a>
+
+			<a class="text-white" href="#">
+				<div class="w-100 bg-secondary py-2 text-center my-4">
+
+				<i class="fas fa-chart-line"></i> Statistics
+
+			</div></a>
+
+
+			<a class="text-light" href="#">
+				<div class="w-100 bg-secondary  py-2 text-center my-4">
+
+				<i class="fas fa-user"></i> Profile
+
+			</div></a>
+
+			
+			<a class="text-white" href="#">
+				<div class="w-100  bg-secondary py-2 text-center my-4">
+
+				<i class="fas fa-envelope"></i> Message Request
+
+			</div></a>
+
+
+			<a class="text-white" href="#">
+				<div class="w-100  bg-secondary py-2 text-center my-4">
+
+				<i class="fab fa-product-hunt"></i> New Product Request
+
+			</div></a>
+
+
+
+		</div>
+
+		
+
+			
+
+		</div>
+
+	</div>
+</div>
+
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="offset-2 col-10">
+			<p class="h2 text-dark">Dashboard</p>
+		</div>
 	</div>
 </div>
 
 
 
+
+
+
+
 <!-- body ends here -->
 <?php 
-include 'linker_files/tail.php';
+include 'linker_files/tail_admin.php';
 ?>
 
 
---
+++ linker_files/head_admin.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <!-- bootstrap cdn -->
+  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+  
+  <!-- bootstrap on server -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+  
+  <!-- google fonts -->
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+
+
+  <!-- font awesome on server -->
+  <link href="font-awesome/css/all.css" rel="stylesheet" type="text/css"/>
+  
+  <link rel="icon" href="img/fevicon.png" type="image/gif" sizes="16x16">
+
+  <title>U-M-A-R-T</title>
+  
+  <!-- font-awesome cdn -->
+  <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
+  
+  <!-- css custom stylesheet -->
+  <link rel="stylesheet" href="css/main.css">
+  
+</head>
+<body>
+
+
+<!-- navigation bar and search bar starts -->
+
+<!-- navigation bar and search bar ends -->
+
+
+  
