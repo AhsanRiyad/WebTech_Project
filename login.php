6334feea02cb1a5845a11f55e8734d3713dcb960
AhsanRiyad <riyad298@gmail.com>
2018-11-11 19:28:07 +0600

    reg button fixed
    reg page will start now

--- login.php
+++ login.php
@@ -30,10 +30,11 @@ include 'linker_files/head.php';
 				
 						</div>
 						<div class="col-12 col-lg-5 align-self-lg-center">
-							<button type="submit" class="btn btn-success rounded-0 w-100 py-2">Log In</button>
+							<button type="submit" name="submit" value="submit" class="btn btn-success rounded-0 w-100 py-2">Log In</button>
 				
 							<p class="text-danger h5 mt-2"><i>Not a member yet?</i></p>
-							<button class="btn btn-primary rounded-0 w-100 py-2" href="reg.php">Register Here</button>
+ 
+							<a href="reg.php"><button type="button" class="btn btn-primary rounded-0 w-100 py-2">Register Here</button></a>
 				
 						</div>
 						
