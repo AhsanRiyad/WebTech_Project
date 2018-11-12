d9e2d6f6bdd93b2507b802abe67057ad60eb8ce3
AhsanRiyad <riyad298@gmail.com>
2018-11-12 13:03:47 +0600

    commit by alias
    this is used for short cut puspose
    
    stable

--- admin_home.php
+++ admin_home.php
@@ -206,64 +206,20 @@ include 'linker_files/head_admin.php';
 										</div>
 									</div>
 								</div>
-
-
-								<div class="row">
-									
-									<div class="col-4 py-5">
-										<table class="table table-hover">
-											<thead>
-												<tr>
-													<th scope="col">#</th>
-													<th scope="col">First</th>
-													<th scope="col">Last</th>
-													<th scope="col">Handle</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<th scope="row">1</th>
-													<td>Mark</td>
-													<td>Otto</td>
-													<td>@mdo</td>
-												</tr>
-												<tr>
-													<th scope="row">2</th>
-													<td>Jacob</td>
-													<td>Thornton</td>
-													<td>@fat</td>
-												</tr>
-												<tr>
-													<th scope="row">3</th>
-													<td colspan="2">Larry the Bird</td>
-													<td>@twitter</td>
-												</tr>
-											</tbody>
-										</table>
-
-
-									</div>
-
-
-								</div>
-
-
-
-
-							</div>
 						</div>
 					</div>
+				</div>
 
 
 
 
-					<!-- admin progress table -->
+				<!-- admin progress table -->
 
 
-					<!-- body ends here -->
-					<?php 
-					include 'linker_files/tail_admin.php';
-					?>
+				<!-- body ends here -->
+				<?php 
+				include 'linker_files/tail_admin.php';
+				?>
 
 
 
