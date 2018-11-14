68a188675addc9404c913b5dfb2454da4b2420ce
AhsanRiyad <riyad298@gmail.com>
2018-11-15 01:21:31 +0600

    mobile number and password done

--- reg.php
+++ reg.php
@@ -4,9 +4,10 @@ include 'linker_files/head.php';
 
 
 <?php
-
+$GLOBALS['gender'] = "";
 $GLOBALS['first_name'] = "";
 $GLOBALS['last_name'] = "";
+$GLOBALS['email'] = "";
 
 //    first name validation
 function first_name_validation() {
@@ -51,8 +52,6 @@ function first_name_validation() {
 	}
 }
 
-
-
 //    last name validation
 function last_name_validation() {
 	if (isset($_POST['submit'])) {
@@ -91,12 +90,124 @@ function last_name_validation() {
 	}
 }
 
+//    email validation
+function email_validation() {
+	if (isset($_POST['submit'])) {
+		$email = $_POST['email'];
+		if ($email == "") {
+			return 'can not be empty';
+			echo '<br>';
+		} else {
+			$email_array = str_split($email, 1);
+			$email_explode = explode('@', $email);
+			if (strlen($email) < 10) {
+				return 'is too short';
+				echo '<br>';
+			} elseif (!in_array('@', $email_array)) {
+				return '@ symbol missing';
+			} elseif (strlen($email_explode[0]) < 5) {
+				return 'you must write smth before @ symbol';
+				echo '<br>';
+			} elseif (substr($email, -3, 1) == '.' || substr($email, -4, 1) == '.') {
 
 
+				if (is_numeric(substr($email, 0, 1)) || substr($email, 0, 1) == '.' || substr($email, 0, 1) == '-') {
+					return 'Last character can not be numeric or character';
+					echo '<br>';
+				} else {
+					foreach ($email_array as $value) {
 
+						if ($value == 'a' || $value == 'b' || $value == 'c' || $value == 'd' || $value == 'e' || $value == 'f' || $value == 'g' || $value == 'h' || $value == 'i' || $value == 'j' || $value == 'k' || $value == 'l' || $value == 'm' || $value == 'n' || $value == 'o' || $value == 'p' || $value == 'q' || $value == 'r' || $value == 's' || $value == 't' || $value == 'u' || $value == 'v' || $value == 'w' || $value == 'x' || $value == 'y' || $value == 'z' || $value == 'A' || $value == 'B' || $value == 'C' || $value == 'D' || $value == 'E' || $value == 'F' || $value == 'G' || $value == 'H' || $value == 'I' || $value == 'J' || $value == 'K' || $value == 'L' || $value == 'M' || $value == 'N' || $value == 'O' || $value == 'P' || $value == 'Q' || $value == 'R' || $value == 'S' || $value == 'T' || $value == 'U' || $value == 'V' || $value == 'W' || $value == 'X' || $value == 'Y' || $value == 'Z' || $value == '.' || $value == '-' || $value == '0' || $value == '1' || $value == '2' || $value == '3' || $value == '4' || $value == '5' || $value == '6' || $value == '7' || $value == '8' || $value == '9' || $value == '@' || $value == '_') {
 
+						} else {
+							return 'Invalid character';
+							echo '<br>';
+						}
+					}
+				}
+			} else {
+				return 'must be under a domain, for example .com or .net etc ';
+				echo '<br>';
+			}
+		}
+	}
+}
 
+//gender validation
+function gender_validation() {
+	if (isset($_POST['submit'])) {
 
+		if (!isset($_POST['gender'])) {
+			return 'One option must be selected';
+			echo '<br>';
+		} else {
+			$GLOBALS['gender'] = $_POST['gender'];
+		}
+	} else {
+
+		return "";
+	}
+}
+
+//password validation
+function password_validation() {
+	if (isset($_POST['submit'])) {
+		if ($_POST['password'] == "" || $_POST['confirm_password'] == "") {
+			return 'You must enter and confirm password';
+			echo '<br>';
+		} else {
+			if ($_POST['password'] != $_POST['confirm_password']) {
+				return 'password does not match';
+				echo '<br>';
+			}
+		}
+	} else {
+		return "";
+	}
+}
+
+//phone number validation
+function phone_validation() {
+	if (isset($_POST['submit'])) {
+		if ($_POST['phone'] == "") {
+			return 'can not be empty';
+			echo '<br>';
+		} else {
+			$phone_array = str_split($_POST['phone'], 1);
+			foreach ($phone_array as $value) {
+				if ($value == '+' || $value == '0' || $value == '1' || $value == '2' || $value == '3' || $value == '4' || $value == '5' || $value == '6' || $value == '7' || $value == '8' || $value == '9') {
+					if (strlen($_POST['phone']) < 12) {
+						return 'too short';
+						echo '<br>';
+						break;
+					}
+				} else {
+					return 'invalid character used';
+					echo '<br>';
+					break;
+				}
+			}
+		}
+	}
+}
+
+//date validation
+function date_validation() {
+	if (isset($_POST['submit'])) {
+		if ($_POST['day'] == 'Day') {
+			return 'you must select a day';
+			echo '<br>';
+		} elseif ($_POST['month'] == 'Month') {
+			return 'you must select a month';
+			echo '<br>';
+		} elseif ($_POST['year'] == 'Year') {
+			return 'you must select a year';
+			echo '<br>';
+		}
+	} else {
+		return "";
+	}
+}
 ?>
 
 
@@ -120,19 +231,51 @@ function last_name_validation() {
 						<!-- email input -->
 						<div class="col-12 col-xl-6 ">
 							<div class="form-group">
-								<label for="exampleInputEmail1"><small>Email address*</small></label>
-								<input  type="text" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+								<label for="exampleInputEmail1"><small>Email address*</small>
+								<br>
+								<?php
+								$st2 = email_validation();
+								if ($st2 != "") {
+									echo "<span style='color: red'>" . $st2 . "</span>" . '<br>';
+								}
+								?>
+									
+								</label>
+								
+
+								<input name="email" type="text" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value="<?php
+								if (isset($_POST['email'])) {
+									echo $_POST['email'];
+								}
+								?>">
 							</div>
 							<!-- password input -->
 							<div class="form-group">
-								<label for="exampleInputPassword1"><small>Password*</small></label>
-								<input type="password" class="form-control rounded-0" id="exampleInputPassword1" placeholder="Password">
+								<label for="exampleInputPassword1"><small>Password*</small>
+									<br>
+									<?php
+									$st = password_validation();
+									if ($st != "") {
+										echo "<span style='color: red;'>" . $st . "</span>" . '<br>';
+									}
+									?>
+
+								</label>
+								<input name="password" type="password" class="form-control rounded-0" id="exampleInputPassword1" placeholder="Password" value="<?php
+								if (isset($_POST['password'])) {
+									echo $_POST['password'];
+								}
+								?>">
 							</div>
 							
 							<!-- re-enter password input -->
 							<div class="form-group mb-4">
 								<label for="exampleInputPassword1"><small>Re-enter password*</small></label>
-								<input type="password" class="form-control rounded-0" id="exampleInputPassword1" placeholder="Password">
+								<input name="confirm_password" type="password" class="form-control rounded-0" id="exampleInputPassword1" placeholder="Password" value="<?php
+								if (isset($_POST['confirm_password'])) {
+									echo $_POST['confirm_password'];
+								}
+								?>" >
 							</div>
 							
 							<!-- month input -->
@@ -285,8 +428,23 @@ function last_name_validation() {
 							
 							<!-- mobile number input -->
 							<div class="form-group mb-xl-3">
-								<label for="exampleInputEmail1"><small>Mobile Number*</small></label>
-								<input  type="text" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter mobile number">
+								<label for="exampleInputEmail1"><small>Mobile Number*</small>
+									<br>
+									<?php
+									$st2 = phone_validation();
+									if ($st2 != "") {
+										echo "<span style='color: red'>" . $st2 . "</span>" . '<br>';
+									}
+									?>
+
+								</label>
+								
+								<input name="phone" type="text" class="form-control rounded-0" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter mobile number"
+								value="<?php
+								if (isset($_POST['phone'])) {
+									echo $_POST['phone'];
+								}
+								?>">
 							</div>
 
 							<!-- toc terms and condition input -->
